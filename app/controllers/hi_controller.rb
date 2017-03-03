98cc66c40a5d0d03c796f0fcd1fd138b4647ccee
Ryan Kliemann <Ryan_blake@icloud.com>
2017-03-03 09:38:37 -0600

    update schema/remove test controller

--- app/controllers/hi_controller.rb
+++
@@ -1,2 +0,0 @@
-class HiController < ApplicationController
-end
--- test/controllers/hi_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class HiControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
